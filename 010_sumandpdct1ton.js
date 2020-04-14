1bfdc476f0073057bd10c6ce18adb45373b06b5a
hima-del <himajaharidas77@gmail.com>
2020-04-14 23:55:53 +0530

    prgrm10 completed

---
+++ 010_sumandpdct1ton.js
@@ -0,0 +1,26 @@
+let readline = require("readline-sync");
+function sum1ton(n) {
+  let total1 = 0;
+  for (let i = 1; i <= n; i++) {
+    total1 += i;
+  }
+  return total1;
+}
+function product1ton(n) {
+  let total2 = 1;
+  for (let i = 1; i <= n; i++) {
+    total2 *= i;
+  }
+  return total2;
+}
+let number1 = parseInt(readline.question("enter the Number :"));
+let option = readline.question(
+  "enter the your choice : s for sum and p for product"
+);
+if (option === "s") {
+  console.log("\n sum :");
+  console.log(sum1ton(number1));
+} else if (option === "p") {
+  console.log("\n product :");
+  console.log(product1ton(number1));
+}
