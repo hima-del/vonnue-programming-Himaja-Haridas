2887cfedef04df189e978a2508c664580aaaea21
hima-del <himajaharidas77@gmail.com>
2020-04-15 23:04:06 +0530

    modified program13

--- 013_odd.js
+++ 013_odd.js
@@ -1,7 +1,10 @@
 let readline = require("readline-sync");
 let number = parseInt(readline.question("enter the Number :"));
-if (number % 2 != 0) {
-  console.log("number is odd");
-} else {
-  console.log("number is not odd");
-}
+const oddCheck = (number) => {
+  if (number % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(oddCheck(number));
