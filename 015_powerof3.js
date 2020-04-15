f01b10faa034bf187c456b87945d3c28a442cba5
hima-del <himajaharidas77@gmail.com>
2020-04-15 23:06:21 +0530

    modified program15

--- 015_powerof3.js
+++ 015_powerof3.js
@@ -1,6 +1,6 @@
 let readline = require("readline-sync");
 let number = readline.question("enter the Number :");
-function powerOfThree(num) {
+const powerOfThree = (num) => {
   for (let i = 0; i < num; i++) {
     if (i * i * i == num) {
       return true;
@@ -8,5 +8,5 @@ function powerOfThree(num) {
       return false;
     }
   }
-}
+};
 console.log(powerOfThree(number));
