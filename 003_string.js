17282657745f7399db0a54be92d4696374a4b3d5
hima-del <himajaharidas77@gmail.com>
2020-04-15 15:03:21 +0530

    modified pgrm3

--- 003_string.js
+++ 003_string.js
@@ -1,19 +1,24 @@
-let string1 = "hello";
-let string2 = "world";
-let string3 = string1.concat(" ", string2);
-console.log(string3);
-
-function reverseString(str) {
+// let string1 = "hello";
+// let string2 = "world";
+// let string3 = string1.concat(" ", string2);
+// console.log(string3);
+const concatenateString = (string1, string2) => {
+  let string3 = string1.concat(" ", string2);
+  return string3;
+};
+let string10 = concatenateString("hello", "world");
+console.log(string10);
+const reverseString = (str) => {
   let splitstring = str.split("");
   let rev = splitstring.reverse();
   let joinstring = rev.join("");
   return joinstring;
-}
+};
 let string4 = reverseString("hello");
 console.log(string4);
 
-let string5 = reverseString(string1);
-let string6 = reverseString(string2);
+let string5 = reverseString("hello");
+let string6 = reverseString("world");
 let string7 = string5.concat(" ", string6);
 console.log(string7);
 
