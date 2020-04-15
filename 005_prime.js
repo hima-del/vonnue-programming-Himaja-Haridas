491b8425d5e43f445323a31d843e5d3fad45f240
hima-del <himajaharidas77@gmail.com>
2020-04-15 16:08:42 +0530

    modified pgrm5

--- 005_prime.js
+++ 005_prime.js
@@ -1,15 +1,10 @@
-function prime(n) {
-  if (n == 1) {
-    return false;
-  } else if (n == 2) {
-    return true;
-  } else {
-    for (let x = 2; x < n; x++) {
-      if (n % x == 0) {
-        return false;
-      }
-      return true;
+const prime = (n) => {
+  for (let x = 2; x < n; x++) {
+    if (n % x == 0) {
+      return false;
     }
+    return true;
   }
-}
+};
+
 console.log(prime(100));
