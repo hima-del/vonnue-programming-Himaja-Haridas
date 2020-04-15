a62112abd216c9da8a82679ecd13be8db412b544
hima-del <himajaharidas77@gmail.com>
2020-04-15 21:34:21 +0530

    modified program7

--- 007_scalarmultiplication.js
+++ 007_scalarmultiplication.js
@@ -1,5 +1,8 @@
 let numbers = [1, 2, 3, 4, 5];
+const scalarMultiplication=(numbers)=>{
 for (let i = 0; i < numbers.length; i++) {
-  numbers[i] = numbers[i] * 2;
+   numbers[i] *=  2;
+  return numbers[i];
 }
-console.log(numbers);
+}
+console.log(scalarMultiplication(numbers));
