1035df324c3bf7f4eb379ca0d320fde61613c345
hima-del <himajaharidas77@gmail.com>
2020-04-15 23:00:05 +0530

    modified program10

--- 010_sumandpdct1ton.js
+++ 010_sumandpdct1ton.js
@@ -1,18 +1,18 @@
 let readline = require("readline-sync");
-function sum1ton(n) {
+const sum1ton = (n) => {
   let total1 = 0;
   for (let i = 1; i <= n; i++) {
     total1 += i;
   }
   return total1;
-}
-function product1ton(n) {
+};
+const product1ton = (n) => {
   let total2 = 1;
   for (let i = 1; i <= n; i++) {
     total2 *= i;
   }
   return total2;
-}
+};
 let number1 = parseInt(readline.question("enter the Number :"));
 let option = readline.question(
   "enter the your choice : s for sum and p for product"
