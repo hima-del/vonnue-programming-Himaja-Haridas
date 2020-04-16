79623a3b0cb7792287a311a050c4d08fe3697d66
hima-del <himajaharidas77@gmail.com>
2020-04-16 10:39:12 +0530

    modified program9

--- 009_summultipleof3and5.js
+++ 009_summultipleof3and5.js
@@ -1,5 +1,5 @@
 let readline = require("readline-sync");
-const sum=(n)=> {
+const sum = (n) => {
   let total = 0;
   for (let i = 0; i <= n; i++) {
     if (i % 3 === 0 || i % 5 === 0 || (i % 3 === 0 && i % 5 === 0)) {
@@ -7,7 +7,7 @@ const sum=(n)=> {
     }
   }
   return total;
-}
+};
 let number1 = parseInt(readline.question("enter the Number :"));
 console.log("\n sum :");
 console.log(sum(number1));
