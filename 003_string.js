c0fef790ed8ef879e55d066f3b10313af1b8c41e
hima-del <himajaharidas77@gmail.com>
2020-04-19 01:20:40 +0530

    modified string program

--- 003_string.js
+++ 003_string.js
@@ -1,53 +1,29 @@
-const readline = require("readline-sync");
+concatenateString = (string1, string2) => {
+  return string1.concat(string2);
+};
 reverseString = (string) => {
   return string.split("").reverse().join("");
 };
-mixLetters = (string1, string2) => {
-  let len = 
-  str1.length > str2.length ? str1.length : str2.length;
-  let combine = "";
-  for (let i = 0; len > i; i++) {
-    if (str1[i] === undefined) {
-      combine = combine + str2[i];
-    } else if (str2[i] === undefined) {
-      combine = combine + str1[i];
-    } else {
-      combine = combine + str1[i] + str2[i];
-    }
-  }
-  return combine;
-};
-splitIntoTwo = (str) => {
-  let len = str.length;
+splitIntoTwo = (string) => {
+  let len = string.length;
   let mid = len / 2;
   let half1, half2;
-  half1 = str.slice(0, mid);
-  half2 = str.slice(mid, len);
+  half1 = string.slice(0, mid);
+  half2 = string.slice(mid, len);
   return half1 + "," + half2;
 };
-removeWhiteSpace = (val) => {
+removeWhiteSpace = (number) => {
   let result = "";
-  for (let i = 0; val.length > i; i++) {
-    if (val[i] !== " ") {
-      result = result + val[i];
+  for (let i = 0; number.length > i; i++) {
+    if (number[i] !== " ") {
+      result = result + number[i];
     }
   }
   return result;
 };
-concatString = (str1, str2) => {
-  return str1.concat(str2);
-};
-let firstString = readline.question("enter first string :");
-let secondString = readline.question("enter second string :");
-console.log("\nconcatenate : " + concatString(firstString, secondString));
-console.log("\nreverse first string: " + reverseStr(firstString));
-console.log("\nreverse second string : " + reverseStr(secondString));
-console.log(
-  "\nreverse concatenate : " +
-    concatString(reverseStr(firstString), reverseStr(secondString))
-);
-console.log("\nmixletters : " + mixLetters(firstString, secondString));
-console.log("\n1st one :split into two : " + splitIntoTwo(firstString));
-console.log("\n2st one :split into two : " + splitIntoTwo(secondString));
-console.log("\nremove whitespaces 1st: " + removeWhiteSpace(firstString));
-console.log("\nremove whitespaces 2nd: " + removeWhiteSpace(secondString));
+
+console.log(concatenateString("hello", "world"));
+console.log(reverseString("hello"));
+console.log(concatenateString(reverseString("hello"), reverseString("world")));
+console.log(splitIntoTwo("world"));
+console.log(removeWhiteSpace("     hello    "));
