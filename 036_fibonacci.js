f7111bb11be20c988870023cf7120f67788f26c8
hima-del <himajaharidas77@gmail.com>
2020-04-18 23:31:26 +0530

    fibonocci  program

--- 036_fibonacci.js
+++ 036_fibonacci.js
@@ -2,11 +2,13 @@ const fibonacciSeries = (n) => {
   let a = 0,
     b = 1,
     f = 1;
+  arr = [];
   for (let i = 2; i <= n; i++) {
     f = a + b;
     a = b;
     b = f;
+    arr.push(b);
   }
-  return f;
+  return arr;
 };
 console.log(fibonacciSeries(50));
