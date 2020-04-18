bdfbeb56d2cefcc044b9ee0500d64a494bd8a41e
hima-del <himajaharidas77@gmail.com>
2020-04-18 18:06:32 +0530

    completed lcm program

--- 026_lcm.js
+++ 026_lcm.js
@@ -1,5 +1,8 @@
 const lcm = (number1, number2) => {
-  let gcd = gcd(number1, number2);
-  return (number1 * number2) / gcd;
+  for (let i = 1; i <= number1 * number2; i++) {
+    if (i % number1 === 0 && i % number2 === 0) {
+      return i;
+    }
+  }
 };
 console.log(lcm(5, 7));
