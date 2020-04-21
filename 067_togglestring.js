03528908d0dc90bbabb1053eadbb05404735b1af
hima-del <himajaharidas77@gmail.com>
2020-04-21 18:16:22 +0530

    toggle string program

---
+++ 067_togglestring.js
@@ -0,0 +1,8 @@
+const toggleString = (str) => {
+  for (let i = 0; str.length!==0; i++) {
+    if (str[i] >= "A" && str[i] <= "Z") str[i] = str[i] + "a" - "A";
+    else if (str[i] >= "a" && str[i] <= "z") str[i] = str[i] + "A" - "a";
+  }
+  return str;
+};
+console.log(toggleString("heLLo"));
