6928fbe1cfa2f82ee85021c1cda8bedf696022e2
hima-del <himajaharidas77@gmail.com>
2020-04-21 15:22:54 +0530

    minor modification  program

--- 014_squaresum.js
+++ 014_squaresum.js
@@ -3,7 +3,7 @@ let number = readline.question("enter the Number :");
 const sumSquare = (num) => {
   for (let i = 1; i * i <= num; i++) {
     for (let j = 1; j * j <= num; j++) {
-      if (i * i + j * j == num) {
+      if (i * i + j * j === num) {
         return true;
       } else {
         return false;
