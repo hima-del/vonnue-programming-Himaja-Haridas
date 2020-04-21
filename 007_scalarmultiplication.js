8c4f46d647a3fca7e143de7ed0038566aec90690
hima-del <himajaharidas77@gmail.com>
2020-04-21 15:17:49 +0530

    minor modification  program

--- 007_scalarmultiplication.js
+++ 007_scalarmultiplication.js
@@ -1,8 +1,8 @@
 let numbers = [1, 2, 3, 4, 5];
-const scalarMultiplication=(numbers)=>{
-for (let i = 0; i < numbers.length; i++) {
-   numbers[i] *=  2;
-  return numbers[i];
-}
-}
+const scalarMultiplication = (numbers) => {
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i] *= 2;
+    return numbers[i];
+  }
+};
 console.log(scalarMultiplication(numbers));
