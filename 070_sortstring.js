e2cf05e63bfb899f4b02b7b3aa45bf60ff1b499b
hima-del <himajaharidas77@gmail.com>
2020-04-21 21:40:16 +0530

    implement sort string program

---
+++ 070_sortstring.js
@@ -0,0 +1,15 @@
+const sortString = (string) => {
+  let array = string.split("");
+  let temp;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array.join("");
+};
+console.log(sortString("hello world"));
