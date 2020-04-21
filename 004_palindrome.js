99bc76f3992c0ffdc0307adbf28531167a415825
hima-del <himajaharidas77@gmail.com>
2020-04-21 15:12:39 +0530

    minor modification  program

--- 004_palindrome.js
+++ 004_palindrome.js
@@ -1,17 +1,11 @@
-const pallindrome = (strentry) => {
+const palindrome = (strentry) => {
   let str = strentry.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
   let count = 0;
-  if (Str.length % 2 == 0) {
-    count = Str.length / 2;
-  } else {
-    count = (Str.length - 1) / 2;
-  }
   for (let x = 0; x < count; x++) {
-    if (Str[x] != Str.slice(-1 - x)[0]) {
+    if (str[x] != str.slice(-1 - x)[0]) {
       console.log("not pallindrome");
       return false;
     }
-
     console.log("pallindrome");
     return true;
   }
