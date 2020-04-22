18eb93af5c177215b98f1f049650d9b4c762d1f6
hima-del <himajaharidas77@gmail.com>
2020-04-22 18:26:04 +0530

    minor modification to program

--- 067_togglestring.js
+++ 067_togglestring.js
@@ -1,7 +1,10 @@
 const toggleString = (str) => {
-  for (let i = 0; str.length!==0; i++) {
-    if (str[i] >= "A" && str[i] <= "Z") str[i] = str[i] + "a" - "A";
-    else if (str[i] >= "a" && str[i] <= "z") str[i] = str[i] + "A" - "a";
+  for (let i = 0; str.length !== 0; i++) {
+    if (str[i] >= "A" && str[i] <= "Z") {
+      str[i] = str[i] + "a" - "A";
+    } else if (str[i] >= "a" && str[i] <= "z") {
+      str[i] = str[i] + "A" - "a";
+    }
   }
   return str;
 };
