d541c9736d9b9b105e94b7e7391c1f55383edf1c
hima-del <61223865+hima-del@users.noreply.github.com>
2020-04-22 16:34:00 +0530

    impliment largest element in a list  program (#51)

---
+++ 063_largestinalist.js
@@ -0,0 +1,11 @@
+let array = [1, 3, 8, 12, 17, 26, 37, 44, 50];
+const largestElement = () => {
+  let largest = 0;
+  for (let i = 0; i <= largest; i++) {
+    if (array[i] > largest) {
+      largest = array[i];
+    }
+  }
+  return largest;
+};
+console.log(largestElement(array));
