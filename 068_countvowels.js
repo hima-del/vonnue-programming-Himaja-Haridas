d7fe17e5c1500eaee6d993c90b20f244a6535267
hima-del <himajaharidas77@gmail.com>
2020-04-22 18:31:56 +0530

    minor modification to program

--- 068_countvowels.js
+++ 068_countvowels.js
@@ -1,13 +1,7 @@
 const countVowel = (string) => {
   let count = 0;
   for (i = 0; i < string.length - 1; i++) {
-    if (
-      string.charAt(i) === "a" ||
-      string.charAt(i) === "e" ||
-      string.charAt(i) === "i" ||
-      string.charAt(i) === "o" ||
-      string.charAt(i) === "u"
-    ) {
+    if (string.charAt(i) === "a" || string.charAt(i) === "e" || string.charAt(i) === "i" || string.charAt(i) === "o" || string.charAt(i) === "u") {
       count += 1;
     }
   }
